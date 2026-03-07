a140d13a6b99318602ec8cf6d33b4b432091ccfd
Roya90 <r_azemi@yahoo.com>
2026-03-06 20:36:13 -0500

    chore: minor fixes

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -5,9 +5,9 @@ export default function HomePageLayout({ children }: PropsWithChildren) {
   return (
     <div className="w-full mx-auto min-h-screen flex flex-col">
       {children}
-      <div className="w-full mx-auto fixed bottom-0 left-1/2 transform -translate-x-1/2">
-        <BottomNavigation />
-      </div>
+      {/* <div className="w-full mx-auto fixed bottom-0 left-1/2 transform -translate-x-1/2"> */}
+      <BottomNavigation />
+      {/* </div> */}
     </div>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,36 @@
+'use client';
+
+import React from 'react';
+import { Box, Typography, Container } from '@mui/material';
+import { Construction } from '@mui/icons-material';
+import { colors } from '@/theme/colors';
+
 const ProfilePage: React.FC = () => {
-  return <div>Profile Page</div>;
+  return (
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '80vh',
+          textAlign: 'center',
+          gap: 2,
+        }}
+      >
+        <Construction sx={{ fontSize: 48, color: colors.text.disabled }} />
+        <Box>
+          <Typography variant="h6" sx={{ fontWeight: 600, color: colors.text.primary }}>
+            Profile coming soon
+          </Typography>
+          <Typography variant="body2" sx={{ color: colors.text.secondary }}>
+            Working on this page. Check back later!
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default ProfilePage;
